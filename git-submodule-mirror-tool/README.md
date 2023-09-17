5b6a27139678f0a0453b2e0db1fa7cf329f804a7
noodlefighter <noodlefighter@gmail.com>
2023-09-17 21:50:29 +0800

    git-submodule-mirror-tool

---
+++ git-submodule-mirror-tool/README.md
@@ -0,0 +1,83 @@
+# git-submodule-mirror-tool
+
+## 介绍
+
+开发项目时引用github等需要代理访问的仓库，但数量一多就难管理，尤其是子模块自身有子模块的情况。
+该脚本可以方便管理子模块的镜像，能一键推送子模块到镜像站、将镜像URL应用到``.gitmodules`文件中。
+
+脚本设计初衷是为了解决CI环境中不方便使用代理的问题。
+
+## 使用方法
+
+1. 开启目标镜像站的`push-to-create`特性，push即创建仓库
+
+以gitea为例，详见[gitea文档](https://docs.gitea.com/administration/config-cheat-sheet#repository-repository)，修改配置文件:
+
+```
+[repository]
+ENABLE_PUSH_CREATE_ORG=true
+ENABLE_PUSH_CREATE_USER=true
+DEFAULT_PUSH_CREATE_PRIVATE=false
+```
+
+2. 编辑`git-mirror.py`，修改镜像站地址
+
+```
+MIRROR_SITE_SSH = "ssh://git@example.com/%s.git"
+MIRROR_SITE_HTTP = "http://example.com/%s"
+```
+
+3. 生成子模块列表至`submodule-mirrors.txt`
+
+```
+$ ./git-mirror.py show > submodule-mirrors.txt
+lib/CGame++/lib/ccronexpr|https://github.com/staticlibs/ccronexpr.git
+lib/CGame++/lib/rapidyaml|https://github.com/noodlefighter/rapidyaml.git
+lib/CGame++/lib/rapidyaml/ext/c4core|https://github.com/biojppm/c4core
+lib/CGame++/lib/rapidyaml/ext/c4core/cmake|https://github.com/biojppm/cmake
+lib/CGame++/lib/rapidyaml/ext/c4core/src/c4/ext/debugbreak|https://github.com/biojppm/debugbreak
+lib/CGame++/lib/rapidyaml/ext/c4core/src/c4/ext/fast_float|https://github.com/fastfloat/fast_float
+
+```
+
+4. 编辑`submodule-mirrors.txt`
+
+根据需要选择保留需要镜像的子模块，并填写镜像的提交路径，如：
+
+```
+lib/CGame++/lib/rapidyaml|https://github.com/noodlefighter/rapidyaml.git|mirror/rapidyaml
+lib/CGame++/lib/rapidyaml/ext/c4core|https://github.com/biojppm/c4core
+lib/CGame++/lib/rapidyaml/ext/c4core/cmake|https://github.com/biojppm/cmake
+```
+
+5. 运行脚本push镜像到指定的镜像站
+
+```
+$ ./git-mirror.py push
+```
+
+6. (可选)将镜像仓库的URL写入当前目录的`.gitmodules`文件
+
+预览`.gitmodules`文件
+
+```
+$ ./git-mirror.py get-gitmodules
+[submodule "lib/CGame++/lib/rapidyaml"]
+	path = lib/CGame++/lib/rapidyaml
+	url = http://example.com/mirror/rapidyaml
+
+[submodule "lib/CGame++/lib/rapidyaml/ext/c4core"]
+	path = lib/CGame++/lib/rapidyaml/ext/c4core
+	url = http://example.com/mirror/c4core
+
+[submodule "lib/CGame++/lib/rapidyaml/ext/c4core/cmake"]
+	path = lib/CGame++/lib/rapidyaml/ext/c4core/cmake
+	url = http://example.com/mirror/c4core-cmake
+```
+
+写入`.gitmodules`文件，然后同步URL到各子模块
+
+```
+$ ./git-mirror.py set-gitmodules
+$ git submodules sync --recursive
+```
---
+++ git-submodule-mirror-tool/git-mirror.py
@@ -0,0 +1,96 @@
+#!/bin/env python3
+
+# 将子模块推送到镜象站、配置gitmodules等
+
+import os
+import argparse
+import subprocess
+
+MIRROR_SITE_SSH = "ssh://git@example.com/%s.git"
+MIRROR_SITE_HTTP = "http://example.com/%s"
+CONFIG_FILE = "submodule-mirrors.txt"
+
+def show_submodules():
+    root_dir = os.getcwd()
+    result = subprocess.run(['git', 'submodule', 'foreach', '--quiet', '--recursive', 'sh', '-c',
+                            f'submodule_path=$(pwd); relative_path=$(realpath --relative-to={root_dir} "$submodule_path"); echo "$relative_path|$(git config --get remote.origin.url)"'],
+                            capture_output=True, text=True)
+    if result.returncode == 0:
+        output = result.stdout.strip()
+        print(output)
+    else:
+        error = result.stderr.strip()
+        print(error)
+
+def print_gitmodules(config_file):
+    with open(config_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                submodule_path, source_url, mirror_url = line.split('|')
+
+                mirror_url = MIRROR_SITE_HTTP % mirror_url
+
+                submodule_path = submodule_path.strip()
+                source_url = source_url.strip()
+                mirror_url = mirror_url.strip()
+
+                # 生成.gitmodules内容
+                gitmodules_content = f"[submodule \"{submodule_path}\"]\n\tpath = {submodule_path}\n\turl = {mirror_url}\n"
+
+                # 打印.gitmodules内容
+                print(gitmodules_content)
+
+def set_gitmodules(config_file):
+    with open(config_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                submodule_path, source_url, mirror_url = line.split('|')
+
+                mirror_url = MIRROR_SITE_HTTP % mirror_url
+
+                submodule_path = submodule_path.strip()
+                source_url = source_url.strip()
+                mirror_url = mirror_url.strip()
+
+                # 更新子模块的URL为镜象仓库URL
+                subprocess.run(['git', 'config', '-f', '.gitmodules', 'submodule.' + submodule_path + '.url', mirror_url])
+
+# 推送到镜象仓库
+def push_mirror(config_file):
+    with open(config_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                submodule_path, source_url, mirror_url = line.split('|')
+
+                submodule_path = submodule_path.strip()
+                mirror_url = MIRROR_SITE_SSH % mirror_url
+
+                source_url = source_url.strip()
+                mirror_url = mirror_url.strip()
+
+                result = subprocess.run(['git', '-C', submodule_path, 'push', '--quiet', '--mirror', mirror_url])
+                if result.returncode == 0:
+                    resultStr = 'OK'
+                else:
+                    resultStr = 'FAIL'
+                print('%s -> %s: %s'%(submodule_path, mirror_url, resultStr))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Git Submodule Mirror Tool')
+    parser.add_argument('command', choices=['show', 'get-gitmodules', 'set-gitmodules', 'push'], help='Command to execute')
+
+    args = parser.parse_args()
+
+    if args.command == 'set-gitmodules':
+        set_gitmodules(CONFIG_FILE)
+    elif args.command == 'get-gitmodules':
+        print_gitmodules(CONFIG_FILE)
+    elif args.command == 'show':
+        show_submodules()
+    elif args.command == 'push':
+        push_mirror(CONFIG_FILE)
